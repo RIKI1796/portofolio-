0402ecd13ac7f7f160c2f4473190f42e68e8f2cd
Hafiz17 <119962175+RIKI1796@users.noreply.github.com>
2025-04-07 09:26:09 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ const navbarMenu = document.getElementById("menu");
 const textElement = document.getElementById("nama");
 const textToType = "Hafiz Tri Septian Ramadhani ";
 let index = 0;
+const translate = document.getElementById("translate");
+const desc = document.getElementById("desc");
+const indo = document.createElement("button");
+indo.classList.add("translate");
+indo.textContent = "Indonesia";
 
 hamburger.addEventListener("click", () => {
   navbarMenu.style.transform = "translateX(0)";
@@ -18,7 +23,7 @@ close.addEventListener("click", () => {
 let isDeleting = false;
 const typingSpeed = 130;
 const deletingSpeed = 100;
-const pauseTime = 2000;  
+const pauseTime = 2000;
 const restartDelay = 1000;
 
 function typeText() {
@@ -35,7 +40,7 @@ function typeText() {
     textElement.textContent = textToType.substring(0, index);
     index++;
     if (index >= textToType.length) {
-      setTimeout(function() {
+      setTimeout(function () {
         isDeleting = true;
         typeText();
       }, pauseTime);
@@ -46,3 +51,15 @@ function typeText() {
 }
 
 typeText();
+
+translate.addEventListener("click", () => {
+  desc.innerHTML =
+    '"Hi, my name is Hafiz Tri Septian Ramadhani. I am currently studying at SMK KRIAN 1. I am majoring in Software Engineering. I have just mastered several programs for web development such as HTML, CSS, and Javascript."';
+  translate.replaceWith(indo);
+});
+
+indo.addEventListener("click", () => {
+  desc.innerHTML =
+    '"Hai namaku Hafiz Tri Septian Ramadhani aku sekarang bersekolah di SMK KRIAN 1. Saya mengambil jurusan Rekayasa Perangkat Lunak. Saya baru menguasai beberapa program untuk web development seperti HTML,CSS, dan Javascript."';
+  indo.replaceWith(translate);
+});
