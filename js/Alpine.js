8da5210cc96c10aa554d5f04aa4c2993b5580b7a
Hafiz17 <119962175+RIKI1796@users.noreply.github.com>
2025-04-07 13:12:56 +0700

    Add files via upload

--- js/Alpine.js
+++ js/Alpine.js
@@ -1,14 +1,27 @@
-document.addEventListener('alpine:init', () => {
-  Alpine.data('project', () => ({
-    items: [{
-      id: 1, name: "Timnas", img: "timnas.jpg", title: "First Project", url: "https://Timnas-indo.vercel.app"
-    },
-    {
-      id: 2, name: "Perpustakaan", img: "perpus.jpg", title: "Second Project", url: "https://perpustakaan-sooty.vercel.app"
-    },
-    {
-      id: 3, name: "Warung Makan", img: "warung.jpg", title: "Third Project", url:"https://warung-makan-alpha.vercel.app"
-    },
-    ]
+document.addEventListener("alpine:init", () => {
+  Alpine.data("project", () => ({
+    items: [
+      {
+        id: 1,
+        name: "Timnas",
+        img: "timnas.jpg",
+        title: "First Project",
+        url: "https://Timnas-indo.vercel.app",
+      },
+      {
+        id: 2,
+        name: "Perpustakaan",
+        img: "perpus.jpg",
+        title: "Second Project",
+        url: "https://perpustakaan-sooty.vercel.app",
+      },
+      {
+        id: 3,
+        name: "Warung Makan",
+        img: "warung.jpg",
+        title: "Third Project",
+        url: "https://warung-makan-alpha.vercel.app",
+      },
+    ],
   }));
 });
